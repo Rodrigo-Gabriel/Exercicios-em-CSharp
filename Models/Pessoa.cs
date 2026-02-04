de4d779f4c26076f941a256af0bd63c78d6dacc9
Rodrigo-Gabriel <rodrigobelchio@gmail.com>
2026-02-04 01:28:25 -0300

    meu primeiro commit

---
+++ Models/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace PessoaApp.Models
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+        public double Altura;
+    }
+}   
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using PessoaApp.Models;
+
+namespace PessoaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            p1.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            p2.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seus dados são: \n Nome: " + p1.Nome + " Idade: " + p1.Idade + " Altura: " + p1.Altura);
+            Console.WriteLine("Seus dados são: \n Nome: " + p2.Nome + " Idade: " + p2.Idade + " Altura: " + p2.Altura);
+
+            // Se p1 for mais velho que p2, exibir uma mensagem indicando isso, caso não, exibir uma mensagem indicando que p2 é mais velho qu p1.
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+        }
+    }
+}
